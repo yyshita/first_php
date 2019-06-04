520ab144f02cfab7a9129e84a55526fb9eb6e3f4
YuyaYamashita <yama.98904@gmail.com>
2019-06-04 19:49:46 +0900

    Add smarty_basic.php

---
+++ first_php/smarty_basic.php
@@ -0,0 +1,2 @@
+<?php
+require_once './vender/autoload.php';
--- first_php/vendor/composer/autoload_static.php
+++ first_php/vendor/composer/autoload_static.php
@@ -7,12 +7,24 @@ namespace Composer\Autoload;
 class ComposerStaticInitd68c35abe80e8f09935dd71913e3fc34
 {
     public static $files = array (
+        '2cffec82183ee1cea088009cef9a6fc3' => __DIR__ . '/..' . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
         'f084d01b0a599f67676cffef638aa95b' => __DIR__ . '/..' . '/smarty/smarty/libs/bootstrap.php',
+    );
+
+    public static $prefixesPsr0 = array (
+        'H' => 
+        array (
+            'HTMLPurifier' => 
+            array (
+                0 => __DIR__ . '/..' . '/ezyang/htmlpurifier/library',
+            ),
+        ),
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixesPsr0 = ComposerStaticInitd68c35abe80e8f09935dd71913e3fc34::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
