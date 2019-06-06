18b46dc50120b741f72fb521c100a16abc6d7360
YuyaYamashita <yama.98904@gmail.com>
2019-06-06 09:20:39 +0900

    Complete smarty_basic.php

--- first_php/smarty_basic.php
+++ first_php/smarty_basic.php
@@ -1,2 +1,7 @@
 <?php
 require_once './vender/autoload.php';
+$s = new Smarty();
+$s->template_dir = './templates/';
+$s->compile_dir = './templates_c/';
+$s->assign('message', 'Hello, World (--)b'); 
+$s->display('smary_basic.tpl');
